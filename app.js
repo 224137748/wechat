624d07d1d38a163c2767f8aa0a4683f48b8208c4
224137748 <snoopy_zuozuo@qq.com>
2020-08-31 22:43:58 +0800

    暂存代码，准备生成js_sdk的singnature

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(auth())
-
 app.use('/users', usersRouter)
+
+app.use(auth())
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
